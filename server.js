d59fe101e5eaaa8d630636087aa92ae34b7595ea
arun <1811054@nec.edu.in>
2020-05-20 20:05:39 +0530

    Lab2 changes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express= require("express")
 var app= express()
 
+var file = require("fs");
+var body_parser= require("body-parser");
+
+app.use(body_parser.json());
+
 var server= app.listen(8081, function(){
     var host= server.address().host;
     var port= server.address().port;
@@ -18,3 +23,24 @@ app.get("/whoami/:name", function(req, res){
     }
     res.end(JSON.stringify(output));
 })
+
+const student={"Name":"AK", "Dept":"Ec", "section":"A"};
+
+app.get("/student",function(req, res){
+
+    const data = file.readFileSync("student.json");
+    const datastr = JSON.parse(data);
+    res.end(JSON.stringify(datastr));
+
+    //res.end(JSON.stringify(student));
+
+})
+
+app.post("/add",function(req,res){
+    var student= req.body;
+
+    console.log("Adding new student :",student);
+    res.end(JSON.stringify("Student Added succssfully"));
+
+})
+
